7a336f9f3f223455d1fccc08b66076753865ba22
chetangupta <chetan.gupta1@housing.com>
2022-07-23 01:22:05 +0530

    wip ui components

---
+++ android/src/main/java/dev/ch8n/android/ui/components/Components.kt
@@ -0,0 +1,35 @@
+package dev.ch8n.android.ui.components
+
+import androidx.compose.runtime.Composable
+import dev.ch8n.android.utils.Preview
+
+//TODO start here
+
+@Composable
+fun ContinueBookmarkCardPreview(isDark: Boolean) {
+    Preview(isDark) {
+        TODO("ContinueBookmarkCard")
+    }
+}
+
+@Composable
+fun ReadingRecommendCardPreview(isDark: Boolean) {
+    Preview(isDark) {
+        TODO("ReadingRecommendCard")
+    }
+}
+
+@Composable
+fun RevisionCardPreview(isDark: Boolean) {
+    Preview(isDark) {
+        TODO("RevisionCard")
+    }
+}
+
+@Composable
+fun BottomNavbarPreview(isDark: Boolean) {
+    Preview(isDark) {
+        TODO("BottomNavbar")
+    }
+}
+
--- android/src/main/java/dev/ch8n/android/utils/UtilsKTX.kt
+++ android/src/main/java/dev/ch8n/android/utils/UtilsKTX.kt
@@ -1,52 +1,20 @@
 package dev.ch8n.android.utils
 
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.core.graphics.toColorInt
+import dev.ch8n.common.ui.theme.BrainMarkTheme
 
 fun String.parseColor(): Color {
     return Color(toColorInt())
 }
 
-fun Modifier.coloredShadow(
-    color: Color,
-    alpha: Float = 0.2f,
-    borderRadius: Dp = 0.dp,
-    shadowRadius: Dp = 20.dp,
-    offsetY: Dp = 0.dp,
-    offsetX: Dp = 0.dp
-) = composed {
-
-    val shadowColor = color.copy(alpha = alpha).toArgb()
-    val transparent = color.copy(alpha = 0f).toArgb()
-
-    this.drawBehind {
-        this.drawIntoCanvas {
-            val paint = Paint()
-            val frameworkPaint = paint.asFrameworkPaint()
-            frameworkPaint.color = transparent
-            frameworkPaint.setShadowLayer(
-                shadowRadius.toPx(),
-                offsetX.toPx(),
-                offsetY.toPx(),
-                shadowColor
-            )
-            it.drawRoundRect(
-                0f,
-                0f,
-                this.size.width,
-                this.size.height,
-                borderRadius.toPx(),
-                borderRadius.toPx(),
-                paint
-            )
-        }
+@Composable
+fun Preview(
+    isDark: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    BrainMarkTheme(isDark) {
+        content.invoke()
     }
 }
