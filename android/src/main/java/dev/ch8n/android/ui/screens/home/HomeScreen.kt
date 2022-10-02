db569cec23727abc5e4e78f85280472a14b8fcb6
Chetan Gupta <chetan.garg36@gmail.com>
2022-10-02 15:40:47 +0530

    Home screen (#23)
    * added revision cards
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip cta bookmark preview
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip preview
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip preview
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * added achived functionality
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip open app share url
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip open app share url
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip open app share url
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * added deeplink
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * code formatted
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * code formatted
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * removed flashcards
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * removed flashcards
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip empty states
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * updated navBar
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * added untagged
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * empty bookmark screen
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * empty bookmark screen
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * themeing update
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * themeing update
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * themeing update
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * themeing update
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * update queries
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * updated data source
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * updated usecases
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * updated usecases
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * added tags paging controller
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * updated paging tags
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip tagmanager screen
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip tagmanager
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip tagmanager
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip tagmanager
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * fixed bookmarks
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    * wip
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>
    
    Signed-off-by: ch8n <chetan.garg36@gmail.com>

--- android/src/main/java/dev/ch8n/android/ui/screens/home/HomeScreen.kt
+++ android/src/main/java/dev/ch8n/android/ui/screens/home/HomeScreen.kt
@@ -1,11 +1,30 @@
 package dev.ch8n.android.ui.screens.home
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
@@ -27,7 +46,12 @@ import dev.ch8n.android.design.components.RecommendedReadCard
 import dev.ch8n.android.utils.toast
 import dev.ch8n.common.data.model.Bookmark
 import dev.ch8n.common.ui.controllers.HomeController
-import dev.ch8n.common.ui.navigation.*
+import dev.ch8n.common.ui.navigation.BookmarksDestination
+import dev.ch8n.common.ui.navigation.CreateBookmarksDestination
+import dev.ch8n.common.ui.navigation.EmptyNavController
+import dev.ch8n.common.ui.navigation.NavController
+import dev.ch8n.common.ui.navigation.PreviewBookmarkHomeDestination
+import dev.ch8n.common.ui.navigation.TagManagerDestination
 import dev.ch8n.common.utils.AndroidPreview
 
 
@@ -80,7 +104,7 @@ fun HomeScreen(
 
         val lastReadBookmarks by controller.lastReadBookmarks.collectAsState()
         val readingRecommendations by controller.readingRecommendations.collectAsState()
-        val revisionBookmarks by controller.revisionBookmarks.collectAsState()
+        val revisionBookmarks by controller.revisionRecommendations.collectAsState()
 
         LaunchedEffect(Unit) {
             controller.getLastReadBookmarks()
